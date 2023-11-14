431fbc7c4f348ded2cca0ff24f5853c827db6743
Su386 <83100266+Su386yt@users.noreply.github.com>
2023-11-13 22:31:54 -0800

    Added onEnable and onDisable function

--- src/main/java/me/su386/coolchaosplugin/CoolChaosPlugin.java
+++ src/main/java/me/su386/coolchaosplugin/CoolChaosPlugin.java
@@ -3,4 +3,14 @@ package me.su386.coolchaosplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CoolChaosPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
 }
